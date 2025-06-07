e07c8b7a2dc3361426ffc5348b7910dc9b14d1bc
estevaoh09 <estevao.ufmg@gmail.com>
2025-06-07 18:44:58 -0300

    Create setores_teste.py

---
+++ classes/Supermercado/setores_teste.py
@@ -0,0 +1,24 @@
+ #temos vinte e oito (28) subsetores no momento
+
+class Setores:
+
+    def limpeza(self, produtos):
+        self.produtos = []
+
+        def add_produto(self, produto):
+            self.produtos.append (produto)
+  
+
+    def hortifruti(self, produtos):
+        self.produtos = []
+
+        def add_produto(self, produto):
+            self.produtos.append (produto)
+  
+
+
+
+supermercado1_setores = Setores ()
+
+supermercado1_setores.limpeza.add_produto("Banana")
+

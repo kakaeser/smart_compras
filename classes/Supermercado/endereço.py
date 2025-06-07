eada3385603ed7143a01b3bb5111d1ea44645ec6
estevaoh09 <estevao.ufmg@gmail.com>
2025-06-07 18:42:38 -0300

    Create endereço.py

---
+++ classes/Supermercado/endereço.py
@@ -0,0 +1,8 @@
+class Endereço:
+    def __init__ (self, pais, estado, cidade, bairro, rua, numero):
+        self.pais = pais
+        self.estado = estado 
+        self.cidade = cidade
+        self.bairro = bairro
+        self.rua = rua
+        self.numero = numero 
